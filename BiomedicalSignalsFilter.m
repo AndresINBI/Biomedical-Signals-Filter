80232220832b152212ba637214fd6f2322c999a4
Andrés Gómez Figueroa <visrt12@gmail.com>
2019-11-24 21:00:13 -0600

    EMG Filtered added
    EMG Added, second signal ready

--- BiomedicalSignalsFilter.m
+++ BiomedicalSignalsFilter.m
@@ -1,9 +1,45 @@
-function PLE1
+function BiomedicalSignalsFilter
 	clc;
 	close all;
+	op=0;
 
+	while (op ~= 4)
+
+		display('Selecciona la señal que quieres analizar:');
+		fprintf('1.-ECG\n2.-EMG\n3.-EEG\n4.-Salir\n');
+		op = input('');   
+		switch op
+			case 1
+				FilterECG();
+				disp('Presiona Enter para cerrar');
+				x=input('');
+				close all;
+				clc;
+				
+			case 2
+				FilterEMG();
+				disp('Presiona Enter para cerrar');
+				x=input('');
+				close all;
+				clc;
+			case 3
+				FilterEEG();
+				disp('Presiona Enter para cerrar');
+				x=input('');
+				close all;
+				clc;
+			case 4
+			otherwise
+				op=0;
+				clc;
+				
+		end
+	end
+end
+
+function FilterECG
 	% Leemos los datos del ECG
-	ECG=csvread('/Users/gomez/Documents/MATLAB/ECG.csv',0,1);
+	ECG=csvread('ECG.csv',0,1);
 	% fs es frecuencia de sampleo, el documento nos indica que fue capurado a 500s/s
 	fs = 500;
 	% N es el numero de datos
@@ -21,41 +57,79 @@ function PLE1
 	h  = fdesign.bandstop('N,F3dB1,F3dB2', 10, 49, 52, fs);
 	Hd = design(h, 'butter');
 	% Aquí se filtra
-	sfilt=filter(Hd,ECG);
+	ECGFilt=filter(Hd,ECG);
 	% Filtro rechazaBanda
 	h  = fdesign.bandstop('N,F3dB1,F3dB2', 10, 150, 152, fs);
 	Hd = design(h, 'butter');
 	% Aquí se filtra
-	sfilt=filter(Hd,sfilt);
+	ECGFilt=filter(Hd,ECGFilt);
 	% Filtro paso bajo
 	h  = fdesign.lowpass('N,F3dB', 5,20, fs);
 	Hd = design(h, 'butter');
 	% Filtrar
-	sfilt=filter(Hd,sfilt);
+	ECGFilt=filter(Hd,ECGFilt);
 
-	TFF=abs(fftshift(fft(sfilt)));
+	TFF=abs(fftshift(fft(ECGFilt)));
 
-
-	graficarECG(T,ECG,sfilt)
-
+	graficar(T,f,ECG,ECGFilt,TF,TFF,'ECG');
 end
 
-function graficarECG(x,y1,y2)
+function FilterEMG
+	% Leemos los datos del EMG
+	EMG=csvread('EMG.csv',0,1);
+	% fs es frecuencia de sampleo, el documento nos indica que fue capurado a 500s/s
+	Fs = 40000;
+	% N es el numero de datos
+	N  = length(EMG);
+	% T es el vector que acomoda los datos en el plot
+	T=1/Fs:1/Fs:N/Fs;
+	% Normalizar paras valores de -1 a 1
+	EMG = EMG/max(abs(EMG));
+	% Tranformada rápida de Fourier
+	TF=abs(fftshift(fft(EMG)));
+	% Vector para acomodar la FFT
+	f=-Fs/2:Fs/(N-1):Fs/2;
+
+	h  = fdesign.highpass('N,F3dB', 10, 100, Fs);
+	Hd = design(h, 'butter');
+	EMGFilt = filter(Hd,EMG);
+	h  = fdesign.lowpass('N,F3dB', 10, 5000, Fs);
+	Hd = design(h, 'butter');
+	EMGFilt = filter(Hd,EMGFilt);
+
+	TFF=abs(fftshift(fft(EMGFilt)));
+
+	graficar(T,f,EMG,EMGFilt,TF,TFF,'EMG')
+end
+
+function FilterEEG
+end
+
+function graficar(x1,x2,y1,y2,y3,y4,titulo)
 	% Graficar la señal sin filtro
 	f1 = figure(1);
-	f1.Name = 'Señal sin Filtro';
+	f1.Name = titulo;
 	f1.NumberTitle = 'off';
-	title('Sin Filtro')
+	subplot(2,1,1)
+	title('Señal')
 	hold('on')
 	grid('on')
 	axis('tight')
 	xlabel('Segundos')
 	ylabel('Amplitud')
-	% spectrogram(ySF)
-	plot(x,y1,'LineWidth',1.5)
-	% periodogram(ySF)
+	plot(x1,y1,'LineWidth',1.5)
+
 	ylim([-1 2])
-	% xlim([0 5])
+
+	subplot(2,1,2)
+	title('Transformada de Fourier')
+	hold('on')
+	grid('on')
+	axis('tight')
+	xlabel('Frecuencia')
+	ylabel('Amplitud')
+	plot(x2,y3,'LineWidth',1.5)
+
 
 	% Figura lado a lado
 	pos1 = get(gcf,'Position');
@@ -63,20 +137,30 @@ function graficarECG(x,y1,y2)
 
 	% % Graficar la señal filtrada
 	f2 = figure(2);
-	f2.Name = 'Señal Filtrada';
+	f2.Name = titulo;
 	f2.NumberTitle = 'off';
-	title('Con Filtro')
+	subplot(2,1,1)
+	title('Señal')
 	hold('on')
 	axis('tight')
 	grid('on')
 	xlabel('Segundos')
 	ylabel('Amplitud')
-	plot(x,y2,'LineWidth',1.5)
-	% spectrogram(yCF)
+	plot(x1,y2,'LineWidth',1.5)
 	ylim([-1 2])
-	% xlim([0 5])
+
+	subplot(2,1,2)
+	title('Transformada Fourier')
+	hold('on')
+	axis('tight')
+	grid('on')
+	xlabel('Frecuencia')
+	ylabel('Amplitud')
+	plot(x2,y4,'LineWidth',1.5)
 
 	% Figura lado a lado
 	pos2 = get(gcf,'Position');
 	set(gcf,'Position', pos2 + [pos1(3)/2,0,0,0])
 end
+
+
